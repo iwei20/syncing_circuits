b80a3e8a9883a10cf3c0d3d0147a22cb8fa31fbf
iwei20 <44007607+iwei20@users.noreply.github.com>
2022-05-25 11:39:29 -0400

    Actually spawn a circuit

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use bevy::prelude::*;
-use syncing_circuits::graphics::DLCPlugin;
+use syncing_circuits::{graphics::{DLCPlugin, DLCBundle, VisualDLC, DLCPosition, DLCSize}, DisconnectedLightbulbCircuit, Rlc};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(start_camera)
+        .add_startup_system(spawn_circuits)
         .add_plugin(DLCPlugin)
         .run();
 
@@ -12,4 +13,12 @@ fn main() {
 
 fn start_camera(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+}
+
+fn spawn_circuits(mut commands: Commands) {
+    commands.spawn_bundle(DLCBundle {
+        vdlc: VisualDLC(DisconnectedLightbulbCircuit::with_constants(5.0, 4.0, 3.0)),
+        position: DLCPosition(Transform::from_translation(Vec3::new(250.0, -250.0, 0.0))),
+        size: DLCSize(Vec2::new(100.0, 100.0))
+    });
 }
