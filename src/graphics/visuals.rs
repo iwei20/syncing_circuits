f12ccd4ae435c41b9e6226d8d7212e965cbdf641
iwei20 <44007607+iwei20@users.noreply.github.com>
2022-05-26 10:40:16 -0400

    Don't think this works

--- src/graphics/visuals.rs
+++ src/graphics/visuals.rs
@@ -1,22 +1,24 @@
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use std::ops::DerefMut;
+
+use bevy::{prelude::*, sprite::{Mesh2dHandle, SpecializedMaterial2d}};
 
 use crate::DisconnectLightCircuitCalculator;
 
 #[derive(Bundle)]
-pub struct DLCBundle {
+pub struct DLCBundle<M: SpecializedMaterial2d> {
     pub dlc: DisconnectLightCircuit,
-    pub position: DLCPosition,
-    pub size: DLCSize
+    pub position: Transform,
+    pub size: DLCSize,
+    pub circuit_light_mesh: (Mesh2dHandle, Mesh2dHandle),
+    pub circuit_light_material: (Handle<M>, Handle<M>)
+
 }
 
 /// Disconnected lightbulb circuit, graphic component
 #[derive(Component)]
 pub struct DisconnectLightCircuit(pub DisconnectLightCircuitCalculator);
 
-#[derive(Component)]
-pub struct DLCPosition(pub Transform);
-
-#[derive(Component)]
+#[derive(Component, Clone)]
 pub struct DLCSize(pub Vec2);
 
 pub struct DLCPlugin;
@@ -25,35 +27,49 @@ pub struct CircuitTimer {
     pub t: f32,
 }
 
+
 impl Plugin for DLCPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
-        app.add_system(draw_dlc);
+
+        app.add_startup_system(spawn_dlc)
+           .add_system(update_lightbulb);
     }
 }
 
-fn draw_dlc(
+
+fn spawn_dlc(
     mut commands: Commands,
-    query: Query<(&DisconnectLightCircuit, &DLCPosition, &DLCSize)>,
-    time: Res<CircuitTimer>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>
-) {
-    for (circuit, position, size) in query.iter() {
-        // Draw circuit
-        commands.spawn_bundle(MaterialMesh2dBundle {
-            mesh: meshes.add(shape::Quad::new(size.0).into()).into(),
-            material: materials.add(ColorMaterial::from(Color::WHITE)),
-            transform: position.0,
-            ..default()
-        });
+) { 
 
-        // Draw lightbulb
-        let light_color = ColorMaterial::from(Color::hsl(0.0, 0.0, 20.0 * circuit.0.lightbulb_power(300.0, time.t)));
-        commands.spawn_bundle(MaterialMesh2dBundle {
-            mesh: meshes.add(shape::Quad::new(size.0 / 4.0).into()).into(),
-            material: materials.add(light_color),
-            transform: position.0 * Transform::from_translation((size.0 / 2.0, 0.0).into()),
-            ..default()
+    let DLC_LIST: [(DisconnectLightCircuit, Transform, DLCSize); 1] = [
+        (
+            DisconnectLightCircuit(DisconnectLightCircuitCalculator::with_constants(0.2, 4.0, 6.0)),
+            Transform::from_translation(Vec3::new(250.0, -250.0, 0.0)),
+            DLCSize(Vec2::new(100.0, 100.0))
+        )
+    ];
+
+    for (circuit, position, size) in DLC_LIST {
+        // Spawn circuit
+        let light_color = ColorMaterial::from(Color::hsl(0.0, 0.0, 20.0 * circuit.0.lightbulb_power(300.0, 0.0)));
+        commands.spawn_bundle(DLCBundle {
+            dlc: circuit,
+            position,
+            size: size.clone(),
+            circuit_light_mesh: (meshes.add(shape::Quad::new(size.0).into()).into(), meshes.add(shape::Quad::new(size.0 / 4.0).into()).into()),
+            circuit_light_material: (materials.add(ColorMaterial::from(Color::WHITE)), materials.add(light_color))
         });
     }
+}
+
+fn update_lightbulb(
+    mut circuit_timer: ResMut<CircuitTimer>,
+    mut query: Query<(&DisconnectLightCircuit, &Handle<ColorMaterial>)>,
+    mut materials: ResMut<Assets<ColorMaterial>>
+) {
+    query.for_each(|(circuit, color)| {
+        materials.get_mut(color).unwrap().color = Color::hsl(0.0, 0.0, 20.0 * circuit.0.lightbulb_power(300.0, circuit_timer.t));
+    });
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use bevy::prelude::*;
-use syncing_circuits::{graphics::{DLCPlugin, DLCBundle, DisconnectLightCircuit, DLCPosition, DLCSize}, DisconnectLightCircuitCalculator, graphics::CircuitTimer};
+use syncing_circuits::{graphics::{DLCPlugin, DisconnectLightCircuit}, graphics::CircuitTimer};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(start_camera)
-        .add_startup_system(spawn_circuits)
         .add_plugin(DLCPlugin)
         .insert_resource(CircuitTimer { t: 0.0 } )
         .add_system(update_time)
@@ -17,24 +16,15 @@ fn start_camera(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 }
 
-fn spawn_circuits(mut commands: Commands) {
-    commands.spawn_bundle(DLCBundle {
-        dlc: DisconnectLightCircuit(DisconnectLightCircuitCalculator::with_constants(0.2, 4.0, 6.0)),
-        position: DLCPosition(Transform::from_translation(Vec3::new(250.0, -250.0, 0.0))),
-        size: DLCSize(Vec2::new(100.0, 100.0))
-    });
-}
-
-
 pub fn update_time(
     mut time: ResMut<CircuitTimer>,
     keyboard_input: Res<Input<KeyCode>>,
-    query: Query<(&DisconnectLightCircuit, &DLCPosition, &DLCSize)>,
+    query: Query<&DisconnectLightCircuit>,
     ) {
     if keyboard_input.pressed(KeyCode::Left) { time.t -= 1.0; }
     if keyboard_input.pressed(KeyCode::Right) { time.t += 1.0; }
     info!("time is now: {}", time.t);
-    for (circuit, _, _) in query.iter() {
+    for circuit in query.iter() {
         info!("power is now: {}", circuit.0.lightbulb_power(10.0, time.t));
      }
 }
