5346e0aec74603e5f03349f4dc5f65b1ca31feba
ok <59588824+jku20@users.noreply.github.com>
2022-05-27 09:37:04 -0400

    refactor front end to use lyon and parents (#2)

--- src/graphics/visuals.rs
+++ src/graphics/visuals.rs
@@ -1,116 +1,102 @@
-use bevy::{prelude::*, sprite::{MaterialMesh2dBundle}};
+use bevy::prelude::*;
+use bevy_prototype_lyon::prelude::*;
+use bevy_prototype_lyon::entity::ShapeBundle;
 
 use crate::DisconnectLightCircuitCalculator;
 
-#[derive(Bundle)]
-pub struct DLCBundle {
-    pub dlc: DisconnectLightCircuit
-}
-
-/// Disconnected lightbulb circuit, graphic component
 #[derive(Component)]
-pub struct DisconnectLightCircuit(pub usize, pub DisconnectLightCircuitCalculator);
-
-#[derive(Component, Clone)]
-pub struct DLCSize(pub Vec2);
+pub struct DLRCCircuit(pub DisconnectLightCircuitCalculator);
+#[derive(Component)]
+pub struct Light;
 
 #[derive(Bundle)]
-pub struct CircuitMesh {
-    pub id: CircuitID,
+pub struct CircuitBundle {
+    pub circuit: DLRCCircuit,
     #[bundle]
-    pub wrapped: MaterialMesh2dBundle<ColorMaterial>
+    pub shape: ShapeBundle,
+    //pub wrapped: MaterialMesh2dBundle<ColorMaterial>
 }
-
-#[derive(Component)]
-pub struct CircuitID(usize);
 
 #[derive(Bundle)]
-pub struct LightMesh {
-    pub id: LightID,
+pub struct LightBundle {
+    pub light: Light,
     #[bundle]
-    pub wrapped: MaterialMesh2dBundle<ColorMaterial>
+    pub shape: ShapeBundle,
+    //pub wrapped: MaterialMesh2dBundle<ColorMaterial>
 }
-
-#[derive(Component)]
-pub struct LightID(usize);
 
 pub struct DLCPlugin;
-
-pub struct CircuitTimer {
-    pub t: f32,
-}
-
 
 impl Plugin for DLCPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
         app.add_startup_system(spawn_dlc)
-           .add_system(update_lightbulb);
+            .insert_resource( CircuitTimer { time: 0.0 } )
+            .add_plugin(ShapePlugin)
+            .add_system(update_lightbulb);
     }
+}
+
+pub struct CircuitTimer {
+    pub time: f32,
 }
 
 
 fn spawn_dlc(
     mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>
 ) { 
+    let dlcc = DLRCCircuit(DisconnectLightCircuitCalculator::with_constants(0.2, 4.0, 6.0));
+    let light_color = Color::hsl(0.0, 0.0, 20.0 * dlcc.0.lightbulb_power(300.0, 0.0));
+    let square = shapes::Rectangle {
+        extents: Vec2::splat(100.0),
+        ..shapes::Rectangle::default()
+    };
+    let circuit_builder = GeometryBuilder::new().add(&square);
+    let light_builder = GeometryBuilder::new().add(&square);
 
-    let DLC_LIST: [(DisconnectLightCircuit, Transform, DLCSize); 1] = [
-        (
-            DisconnectLightCircuit(
-                0,
-                DisconnectLightCircuitCalculator::with_constants(0.2, 4.0, 6.0)
-            ),
-            Transform::from_translation(Vec3::new(250.0, -250.0, 0.0)),
-            DLCSize(Vec2::new(100.0, 100.0))
-        )
-    ];
-
-    for (circuit, position, size) in DLC_LIST {
-        // Spawn circuit
-        let light_color = ColorMaterial::from(Color::hsl(0.0, 0.0, 20.0 * circuit.1.lightbulb_power(300.0, 0.0)));
-        commands.spawn_bundle(CircuitMesh {
-            id: CircuitID(circuit.0),
-            wrapped: MaterialMesh2dBundle { 
-                mesh: meshes.add(shape::Quad::new(size.0).into()).into(), 
-                material: materials.add(ColorMaterial::from(Color::WHITE)), 
-                transform: position, 
-                ..default()
+    commands.spawn_bundle(
+        CircuitBundle {
+            circuit: dlcc,
+            shape: 
+                circuit_builder.build(
+                    DrawMode::Outlined {
+                        fill_mode: FillMode::color(Color::WHITE),
+                        outline_mode: StrokeMode::new(Color::BLACK, 5.0),
+                    },
+                    Transform::from_translation(Vec3::new(250.0, -250.0, 0.0)),
+                )
+        }
+    ).with_children(|parent| {
+        parent.spawn_bundle(
+            LightBundle {
+                light: Light { },
+                shape: 
+                    light_builder.build(
+                        DrawMode::Outlined {
+                            fill_mode: FillMode::color(light_color),
+                            outline_mode: StrokeMode::new(Color::BLACK, 5.0),
+                        },
+                        Transform::from_translation(Vec3::new(0.0, 100.0, 0.0)),
+                    )
             }
-        });
-
-        commands.spawn_bundle(LightMesh {
-            id: LightID(circuit.0),
-            wrapped: MaterialMesh2dBundle { 
-                mesh: meshes.add(shape::Quad::new(size.0 / 4.0).into()).into(), 
-                material: materials.add(light_color), 
-                transform: position * Transform::from_translation((size.0 / 2.0, 0.0).into()), 
-                ..default()
-            }
-        });
-
-        commands.spawn_bundle(DLCBundle {
-            dlc: circuit,
-        });
-    }
+        );
+    });
 }
 
 fn update_lightbulb(
     circuit_timer: ResMut<CircuitTimer>,
-    query_circuits: Query<&DisconnectLightCircuit>,
-    query_light_meshes: Query<(&LightID, &Handle<ColorMaterial>)>,
-    mut materials: ResMut<Assets<ColorMaterial>>
+    mut query_lights: Query<(&Light, &Parent, &mut DrawMode)>,
+    query_circs: Query<&DLRCCircuit>,
 ) {
-    query_circuits.for_each(|circuit| {
-        query_light_meshes.for_each(|(id, handle)| {
-            if id.0 == circuit.0 {
-                materials.get_mut(handle).unwrap().color = 
-                    Color::hsl(
-                        0.0,
-                        0.0,
-                        20.0 * circuit.1.lightbulb_power(300.0, circuit_timer.t)
-                    );
-            }
-        });
-    });
+    for (_, parent, mut draw_mode) in query_lights.iter_mut() {
+        let parent_circuit = query_circs.get(parent.0).expect("couldn't find child to light");
+        let new_power = parent_circuit.0.lightbulb_power(300.0, circuit_timer.time);
+        let new_color = Color::hsl(0.0, 0.0, 20.0 * new_power);
+        if let DrawMode::Outlined {
+            ref mut fill_mode,
+            ref mut outline_mode,
+        } = *draw_mode
+        {
+            fill_mode.color = new_color;
+        }
+    }
 }
