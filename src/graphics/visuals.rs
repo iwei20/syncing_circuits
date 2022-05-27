5e34c28156e8e434f48cf9d462994c21cae76c38
ok <59588824+jku20@users.noreply.github.com>
2022-05-27 09:40:13 -0400

    Rlc backend (#3)
    * implement rlc circuit
    
    * Make rlc fields public
    
    * Create lightbulb loop
    
    * Make DisconnectedLightbulbCircuit available
    
    * Structure cleanup
    
    * Begin to figure out bevy
    
    * Make query immutable
    
    * Add a Size component
    
    * Make a bundle for DLC
    
    * Change DLCPlugin to non-startup system to properly draw
    
    * Create a bundle
    
    * Actually spawn a circuit
    
    * Remove unused import
    
    * Renaming things to make more sense
    
    * experment (wip)
    
    * Create flashing light
    
    * Remove unused import
    
    * Don't think this works
    
    * Do nested bundle thing
    
    * Tried wrapping meshes, but they kind of don't spawn
    
    * Fix update query
    
    * Nested bundle no workie either
    
    * Add ids instead of bundling
    
    * On update, match id and change color
    
    * Fix wrong tuple
    
    * Fix update_time assumed charge
    
    * refactor front end to use lyon and parents (#2)
    
    Co-authored-by: iwei20 <44007607+iwei20@users.noreply.github.com>

--- src/graphics/visuals.rs
+++ src/graphics/visuals.rs
@@ -1,59 +1,102 @@
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use bevy::prelude::*;
+use bevy_prototype_lyon::prelude::*;
+use bevy_prototype_lyon::entity::ShapeBundle;
 
 use crate::DisconnectLightCircuitCalculator;
 
+#[derive(Component)]
+pub struct DLRCCircuit(pub DisconnectLightCircuitCalculator);
+#[derive(Component)]
+pub struct Light;
+
 #[derive(Bundle)]
-pub struct DLCBundle {
-    pub dlc: DisconnectLightCircuit,
-    pub position: DLCPosition,
-    pub size: DLCSize
+pub struct CircuitBundle {
+    pub circuit: DLRCCircuit,
+    #[bundle]
+    pub shape: ShapeBundle,
+    //pub wrapped: MaterialMesh2dBundle<ColorMaterial>
 }
 
-/// Disconnected lightbulb circuit, graphic component
-#[derive(Component)]
-pub struct DisconnectLightCircuit(pub DisconnectLightCircuitCalculator);
-
-#[derive(Component)]
-pub struct DLCPosition(pub Transform);
-
-#[derive(Component)]
-pub struct DLCSize(pub Vec2);
+#[derive(Bundle)]
+pub struct LightBundle {
+    pub light: Light,
+    #[bundle]
+    pub shape: ShapeBundle,
+    //pub wrapped: MaterialMesh2dBundle<ColorMaterial>
+}
 
 pub struct DLCPlugin;
 
-pub struct CircuitTimer {
-    pub t: f32,
-}
-
 impl Plugin for DLCPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
-        app.add_system(draw_dlc);
+        app.add_startup_system(spawn_dlc)
+            .insert_resource( CircuitTimer { time: 0.0 } )
+            .add_plugin(ShapePlugin)
+            .add_system(update_lightbulb);
     }
 }
 
-fn draw_dlc(
-    mut commands: Commands,
-    query: Query<(&DisconnectLightCircuit, &DLCPosition, &DLCSize)>,
-    time: Res<CircuitTimer>,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>
-) {
-    for (circuit, position, size) in query.iter() {
-        // Draw circuit
-        commands.spawn_bundle(MaterialMesh2dBundle {
-            mesh: meshes.add(shape::Quad::new(size.0).into()).into(),
-            material: materials.add(ColorMaterial::from(Color::WHITE)),
-            transform: position.0,
-            ..default()
-        });
+pub struct CircuitTimer {
+    pub time: f32,
+}
 
-        // Draw lightbulb
-        let light_color = ColorMaterial::from(Color::hsl(0.0, 0.0, 20.0 * circuit.0.lightbulb_power(300.0, time.t)));
-        commands.spawn_bundle(MaterialMesh2dBundle {
-            mesh: meshes.add(shape::Quad::new(size.0 / 4.0).into()).into(),
-            material: materials.add(light_color),
-            transform: position.0 * Transform::from_translation((size.0 / 2.0, 0.0).into()),
-            ..default()
-        });
+
+fn spawn_dlc(
+    mut commands: Commands,
+) { 
+    let dlcc = DLRCCircuit(DisconnectLightCircuitCalculator::with_constants(0.2, 4.0, 6.0));
+    let light_color = Color::hsl(0.0, 0.0, 20.0 * dlcc.0.lightbulb_power(300.0, 0.0));
+    let square = shapes::Rectangle {
+        extents: Vec2::splat(100.0),
+        ..shapes::Rectangle::default()
+    };
+    let circuit_builder = GeometryBuilder::new().add(&square);
+    let light_builder = GeometryBuilder::new().add(&square);
+
+    commands.spawn_bundle(
+        CircuitBundle {
+            circuit: dlcc,
+            shape: 
+                circuit_builder.build(
+                    DrawMode::Outlined {
+                        fill_mode: FillMode::color(Color::WHITE),
+                        outline_mode: StrokeMode::new(Color::BLACK, 5.0),
+                    },
+                    Transform::from_translation(Vec3::new(250.0, -250.0, 0.0)),
+                )
+        }
+    ).with_children(|parent| {
+        parent.spawn_bundle(
+            LightBundle {
+                light: Light { },
+                shape: 
+                    light_builder.build(
+                        DrawMode::Outlined {
+                            fill_mode: FillMode::color(light_color),
+                            outline_mode: StrokeMode::new(Color::BLACK, 5.0),
+                        },
+                        Transform::from_translation(Vec3::new(0.0, 100.0, 0.0)),
+                    )
+            }
+        );
+    });
+}
+
+fn update_lightbulb(
+    circuit_timer: ResMut<CircuitTimer>,
+    mut query_lights: Query<(&Light, &Parent, &mut DrawMode)>,
+    query_circs: Query<&DLRCCircuit>,
+) {
+    for (_, parent, mut draw_mode) in query_lights.iter_mut() {
+        let parent_circuit = query_circs.get(parent.0).expect("couldn't find child to light");
+        let new_power = parent_circuit.0.lightbulb_power(300.0, circuit_timer.time);
+        let new_color = Color::hsl(0.0, 0.0, 20.0 * new_power);
+        if let DrawMode::Outlined {
+            ref mut fill_mode,
+            ref mut outline_mode,
+        } = *draw_mode
+        {
+            fill_mode.color = new_color;
+        }
     }
 }
