d17c912e7c069c5cdbb925c64684f73512ddb47f
Ivan Wei <44007607+iwei20@users.noreply.github.com>
2022-05-31 11:32:58 -0400

    Add fading out circles on peaks (#9)
    * Make angular frequency calculation public
    
    * Circle spawning at peak
    
    * Lower epsilon so only one circle is spawned
    
    * Made expanding circles
    
    * Transparent circles!
    
    * Add linear fade out

--- src/graphics/visuals.rs
+++ src/graphics/visuals.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy_prototype_lyon::{entity::ShapeBundle, prelude::{GeometryBuilder, DrawMode, FillMode, StrokeMode, Path, ShapePath}, shapes, plugin::ShapePlugin};
 
 use crate::DisconnectLightCircuitCalculator;
 use std::cmp::PartialEq;
@@ -10,6 +11,9 @@ pub struct DLRCCircuit(pub DisconnectLightCircuitCalculator);
 #[derive(Component)]
 /// A marker component to indicate this shape is a light.
 pub struct Light;
+
+#[derive(Component)]
+pub struct CircleRadius(pub f32);
 
 #[derive(Bundle)]
 pub struct CircuitBundle {
@@ -25,17 +29,26 @@ pub struct LightBundle {
     pub sprite_sheet_bundle: SpriteSheetBundle,
 }
 
+#[derive(Bundle)]
+pub struct CircleBundle {
+    pub radius: CircleRadius,
+    #[bundle]
+    pub shape_bundle: ShapeBundle
+}
 /// This plugin spawns all disconnected lightbulb circuits, adds a shared manipulable timer to the resources, and updates the lightbulb brightness.
 pub struct DLCPlugin;
 
 impl Plugin for DLCPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
-        app.add_startup_system(spawn_dlc)
+        app
+            .add_plugin(ShapePlugin)
+            .add_startup_system(spawn_dlc)
             .insert_resource(CircuitTimer {
                 time: MIN_CIRCUIT_TIME,
                 mode: CircuitTimerMode::Pause,
             })
-            .add_system(update_lightbulb);
+            .add_system(update_lightbulb)
+            .add_system(expand_circles);
     }
 }
 
@@ -91,10 +104,12 @@ fn spawn_dlc(
                 },
             });
         });
+        
 }
 
 /// Updates the colors of all light entities based on the time provided by CircuitTimer.
 fn update_lightbulb(
+    mut commands: Commands,
     circuit_timer: ResMut<CircuitTimer>,
     mut query_lights: Query<(&Parent, &mut TextureAtlasSprite), With<Light>>,
     query_circs: Query<&DLRCCircuit>,
@@ -104,6 +119,7 @@ fn update_lightbulb(
             .get(parent.0)
             .expect("couldn't find child to light");
         let new_power = parent_circuit.0.lightbulb_power(circuit_timer.time);
+
         if new_power < 0.05 {
             sprite.index = 0;
         } else if new_power < 0.1 {
@@ -113,5 +129,56 @@ fn update_lightbulb(
         } else {
             sprite.index = 3;
         }
+
+        // Check if the current time (phase shifted) is a multiple of a half period
+        let epsilon = 0.003;
+        let period = std::f32::consts::TAU / parent_circuit.0.circuit.angular_freq();
+        let time_to_peaks = period / 2.0;
+        let time_multiple = (circuit_timer.time + period / 4.0) / time_to_peaks;
+        let closest_integer_multiple = time_multiple.round();
+        if (time_multiple - closest_integer_multiple).abs() < epsilon && closest_integer_multiple != 0.0 {
+            info!("Circle spawned");
+            let starting_radius = 0.0;
+            let circle_builder = GeometryBuilder::new().add(&shapes::Circle {
+                radius: starting_radius,
+                ..shapes::Circle::default()
+            });
+            commands
+                .entity(**parent)
+                .with_children(|parent| {
+                    parent.spawn_bundle(
+                        CircleBundle {
+                            radius: CircleRadius(starting_radius),
+                            shape_bundle: circle_builder.build(
+                                DrawMode::Outlined {
+                                    fill_mode: FillMode::color(Color::hsla(0.0, 0.0, 0.0, 0.0)),
+                                    outline_mode: StrokeMode::new(Color::hsla(0.0, 0.0, 1.0, calculate_circle_alpha(starting_radius)), 1.0),
+                                },
+                                Transform::from_translation(Vec3::new(0.0, 25.0, 20.0))
+                            )
+                        }
+                    );
+                });
+        }
+        
+    }
+}
+
+fn calculate_circle_alpha(radius: f32) -> f32 {
+    (-radius / 100.0) + 1.0
+}
+
+fn expand_circles(mut query: Query<(&mut CircleRadius, &mut Path, &mut DrawMode)>) {
+    for (mut radius, mut path, mut draw_mode) in query.iter_mut() {
+        *radius = CircleRadius(radius.0 + 0.2);
+        let new_circle = shapes::Circle {
+            radius: radius.0,
+            ..shapes::Circle::default()
+        };
+        *draw_mode = DrawMode::Outlined {
+            fill_mode: FillMode::color(Color::hsla(0.0, 0.0, 0.0, 0.0)),
+            outline_mode: StrokeMode::new(Color::hsla(0.0, 0.0, 1.0, calculate_circle_alpha(radius.0)), 1.0),
+        };
+        *path = ShapePath::build_as(&new_circle);
     }
 }
