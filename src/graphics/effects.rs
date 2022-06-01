cefd8f4a9d1a9e0ffea8c02198592d2124853e2f
iwei20 <44007607+iwei20@users.noreply.github.com>
2022-06-01 11:16:08 -0400

    Add implements

--- src/graphics/effects.rs
+++ src/graphics/effects.rs
@@ -1,20 +1,31 @@
-use bevy::{render::render_resource::{ShaderStage, ShaderStages}, prelude::Plugin};
+use bevy::{render::{render_resource::{ShaderStage, ShaderStages}, render_asset::RenderAsset}, prelude::Plugin, sprite::Material2d, asset::Asset, reflect::TypeUuid};
 
 pub struct EffectsPlugin;
 
 impl Plugin for EffectsPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
-        app.add_startup_system(setup_background);
     }
 }
 
-/// A transparent material that should scramble background?
+/// A transparent material that should scramble background
+#[derive(TypeUuid)]
+#[uuid = "cd3d98e9-bc74-4e0b-9f2e-cd9372bfcdcb"]
 pub struct NoiseMaterial;
 
-pub fn setup_background(
-    commands: &mut Commands,
-    mut pipelines: ResMut<Assets<PipelineDescriptor>>,
-    mut shaders: ResMut<Assets<Shader>>,
-    window: Res<WindowDescriptor>,
-) {
+impl Material2d for NoiseMaterial {
+    fn bind_group(material: &<Self as RenderAsset>::PreparedAsset) -> &bevy::render::render_resource::BindGroup {
+        todo!()
+    }
+
+    fn bind_group_layout(render_device: &bevy::render::renderer::RenderDevice) -> bevy::render::render_resource::BindGroupLayout {
+        todo!()
+    }
+}
+
+impl Asset for NoiseMaterial {
+
+}
+
+impl RenderAsset for NoiseMaterial {
+
 }
